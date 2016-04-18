a6a576d13be0566b613a7672d331383d9728cdb9
kalilo <kalilohansmann@gmail.com>
2016-04-18 08:10:19 -0400

    added comments

--- reduction_linked.c
+++ reduction_linked.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   reduction_linked.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khansman, ghavenga <marvin@42.fr>          +#+  +:+       +#+        */
+/*   By: khansman, ghavnga <marvin@42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/17 16:23:13 by khansman          #+#    #+#             */
 /*   Updated: 2016/04/17 13:24:00 by khansman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-include "bsq.h"
+include "header.h"
 
 //this file is to implement the linked list reduction method.
 tsquare	ft_scan_map_reduction(int fd)
