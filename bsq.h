7e21d2ab05d3e311c02825d008d0708281c2e7b6
Gman513 <gregory.havenga@gmail.com>
2016-04-20 14:29:03 +0200

    readded header file

---
+++ bsq.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsq.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khansman, ghavenga                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/12 13:05:22 by khansman          #+#    #+#             */
+/*   Updated: 2016/04/13 13:42:00 by khansman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __BSQ_H__
+#define __BSQ_H__
+
+#define UINT unsigned int
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+typedef struct	s_square
+{
+	unsigned int	start_x;
+	unsigned int	start_y;
+	unsigned int	size;
+}				t_square;
+
+typedef	struct	s_map_elem
+{
+	char				delimiter;
+	unsigned int		num_empty;
+	unsigned int		num_delim;
+	struct s_map_elem	*next;
+}				t_map_elem;
+
+
+typedef	struct	s_map_info
+{
+	unsigned int	map_lines;
+	unsigned int	line_len;
+	char			empty;
+	char			obstacle;
+	char			full;
+	unsigned int	start_pos;
+}				t_map_info;
+
+/*"The following global variables are accessed multiple times throughout the 
+programs functions, making it an unnecessary use of cpu cycles and memory to 
+repeatedly pass them back and forth between functions."*/
+tmap_info	map_info;//contains info on the current map.
+tmap_elem	*map_list;//pointer to the start of our linked list
+int			fd;
+
+#endif
