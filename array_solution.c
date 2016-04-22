aab2d4a77fe3c437da5c03bc175400ecb7c768df
kalilo <kalilohansmann@gmail.com>
2016-04-22 04:40:05 -0400

    fixed logical error involving && used instead of ||

--- array_solution.c
+++ array_solution.c
@@ -70,13 +70,13 @@ int	ft_set_array(void)
 	
 	k = 0;
 	l = 0;
-	if (map_info.line_len == 0/* || buff != '\n'*/)
+	if (map_info.line_len == 0)
 		return (0);
 	//arr_row = malloc(sizeof(char) * map_info.line_len);
 	arr_col = malloc(sizeof(char) * map_info.line_len * map_info.map_lines);//malloc(sizeof(arr_row))
 	while(read(fd, &buff, 1) && k <= map_info.map_lines)
 	{
-		if (buff != (map_info.empty || map_info.obstacle || '\n'))
+		if (buff != map_info.empty && buff != map_info.obstacle && buff != '\n')
 			return (0);
 		if (buff != '\n')
 			arr_col[k][l] = buff;
